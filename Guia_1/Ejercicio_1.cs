22545f007ba925d62ae04524824b02c40abc37f5
Clegro2003 <carloslegro6@gmail.com>
2025-02-19 19:59:07 -0500

    Primer commit

---
+++ Guia_1/Ejercicio_1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_1
+{
+    public class Ejercicio_1
+    {
+        //constantes
+        const float PORC_NOTA1 = 0.3F;
+        const float PORC_NOTA2 = 0.3F;
+        const float PORC_NOTA3 = 0.4F;
+
+        //variables
+        public string nombre;
+        public float nota1;
+        public float nota2;
+        public float nota3;
+
+        //funciones
+        public void CapturarNotas()
+        {
+            Console.Clear();
+            Console.WriteLine("SISTEMA DE GESTION DE NOTAS");
+
+            Console.Write("Nombre Estudiante: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("NOTA 1: ");
+            nota1 = float.Parse(Console.ReadLine());
+
+            Console.Write ("NOTA 2: ");
+            nota2 = float.Parse(Console.ReadLine());
+
+            Console.Write ("NOTA 3: ");
+            nota3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("datos cargados correctamente");
+            Console.ReadKey();
+
+        }
+
+        private float CalcularPromedio()
+        {
+            //float promedio;
+            //promedio = nota1 * PORC_NOTA1 + nota2 * PORC_NOTA2 + nota3 * PORC_NOTA3;
+            //return promedio;
+
+            return nota1 * PORC_NOTA1 + nota2 * PORC_NOTA2 + nota3 * PORC_NOTA3;
+        }
+
+        private string EstadoEstudiante()
+        {
+            if (CalcularPromedio()>=3)
+            {
+                return "Eres un Estudiante Feliz ...";
+            }
+            return "siga intentando ...";
+        }
+
+        public void ImprimirResultado()
+        {
+            Console.Clear();
+            Console.WriteLine("SISTEMA DE GESTION DE NOTAS");
+            Console.WriteLine("Nombre: " + nombre);
+            //Console.WriteLine(nombre);
+            Console.WriteLine($"Primer Parcial {nota1.ToString("0.0")}");
+            //Console.WriteLine(nota1);
+            Console.Write("Segundo Parcial ");
+            Console.WriteLine(nota2.ToString("0.0"));
+            Console.Write("Examen Final ");
+            Console.WriteLine(nota3.ToString("0.00"));
+            Console.Write("DEfinitiva ");
+            Console.WriteLine(CalcularPromedio().ToString("0.00"));
+            Console.WriteLine(EstadoEstudiante());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Guia_1/MenuPrincipal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_1
+{
+    public class MenuPrincipal
+    {
+        
+        public void Menu()
+        {
+            int op;
+            do
+            {
+                 Console.Clear();
+                Console.SetCursorPosition(10,2); Console.Write("M E N U   P R I N C I P A L");
+                Console.SetCursorPosition(12,4); Console.Write("1. Gestion de Notas");
+                Console.SetCursorPosition(12, 5); Console.Write("2. Calcular el número de pulsaciones ");
+                Console.SetCursorPosition(12, 7); Console.Write("3. información financiera ");
+                Console.SetCursorPosition(12, 10); Console.Write("9. Salir ");
+
+                Console.SetCursorPosition(12, 19); Console.Write("digite una opcion: ");
+                Console.SetCursorPosition(32, 19); op = int.Parse(Console.ReadLine());
+
+
+                switch (op)
+                {
+                    case 1:
+                        //MenuEjecicio1();
+                        Ejercicio_1 ejercicio_1 = new Ejercicio_1();
+                        ejercicio_1.CapturarNotas();
+                        ejercicio_1.ImprimirResultado();
+                        //Console.WriteLine( " aqui va el codigo del menu de ej1");
+                        break;
+                    case 2:
+                        Console.WriteLine("el code ej2");
+                        break;
+                    case 3:
+                        Console.WriteLine("code eje3");
+                        break;
+                    case 9:
+                        Console.Clear();
+                        Console.WriteLine("gracias por usar productos johnp");
+                        Console.ReadKey();
+                        break;
+                }
+
+            } while (op!=9);
+
+        }
+    }
+}
